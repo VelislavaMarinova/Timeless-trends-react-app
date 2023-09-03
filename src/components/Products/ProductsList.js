542805de3ccb7c6b306d90456164810387124bfe
Vel MAr <velmar.annabell@gmail.com>
2023-09-03 18:23:59 +0300

    correct useHttp

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -10,8 +10,8 @@ import ProductHeader from "./ProductHeader";
 
 
 const ProductsList = () => {
-    
-    const url=`http://localhost:3200/products?_embed=reviews&category=${category}&_page=${page}&_limit=${limit}`
+
+    //const url=`http://localhost:3200/products?_embed=reviews&category=${category}&_page=${page}&_limit=${limit}`
 
     // let page=1
     // const [products, setProducts] = useState([]);
@@ -51,45 +51,46 @@ const ProductsList = () => {
     // console.log(loadMore);
     // console.log('render');
 
-    return (
-        <div className="products container">
-            <div className="product-list-loader">
+    // return (
+    //     // <div className="products container">
+    //     //     <div className="product-list-loader">
 
-            </div>
-            <div className="product-list-wrapper">
-                <div className="product-list-header-wrapper">
+    //     //     </div>
+    //     //     <div className="product-list-wrapper">
+    //     //         <div className="product-list-header-wrapper">
 
-                    {/* <ProductHeader category={category}></ProductHeader> */}
-                    <div className="product-list-header-loaded-products">
-                        <select
-                            className="product-list-select-products-per-page"
-                            value={limit} onChange={e => setLimit(e.target.value)}
-                        >
-                            <option value="8">8 per page</option>
-                            <option value="16">16 per page</option>
-                        </select>
-                        <span >category totalProducts
-                        </span>
-                        <p > products filtered by </p>
+    //     //             {/* <ProductHeader category={category}></ProductHeader> */}
+    //     //             <div className="product-list-header-loaded-products">
+    //     //                 <select
+    //     //                     className="product-list-select-products-per-page"
+    //     //                     value={limit} onChange={e => setLimit(e.target.value)}
+    //     //                 >
+    //     //                     <option value="8">8 per page</option>
+    //     //                     <option value="16">16 per page</option>
+    //     //                 </select>
+    //     //                 <span >category totalProducts
+    //     //                 </span>
+    //     //                 <p > products filtered by </p>
 
-                    </div>
-                    <div className="product-list-sort" >
-                    </div>
-                </div>
-                <div className="product-list">
-                    <div className="product-list-filters">
-                    </div>
-                    <div className="product-list-cards">
-                        <ul className="all-cards">
-                            {products.map(p => <li className="product" key={p.id} >  <ProductItem product={p}></ProductItem></li>)}
-                        </ul>
-                        <button onClick={onLoadMore} disabled={!loadMore}>Load More</button>
-                    </div>
-                </div>
-                <div className="product-list-ni-products">
-                    <p className="no-products">No products in category category!<i className="fa-solid fa-magnifying-glass"></i></p>
-                </div>
-            </div>
-        </div>)
+    //     //             </div>
+    //     //             <div className="product-list-sort" >
+    //     //             </div>
+    //     //         </div>
+    //     //         <div className="product-list">
+    //     //             <div className="product-list-filters">
+    //     //             </div>
+    //     //             <div className="product-list-cards">
+    //     //                 <ul className="all-cards">
+    //     //                     {products.map(p => <li className="product" key={p.id} >  <ProductItem product={p}></ProductItem></li>)}
+    //     //                 </ul>
+    //     //                 <button onClick={onLoadMore} disabled={!loadMore}>Load More</button>
+    //     //             </div>
+    //     //         </div>
+    //     //         <div className="product-list-ni-products">
+    //     //             <p className="no-products">No products in category category!<i className="fa-solid fa-magnifying-glass"></i></p>
+    //     //         </div>
+    //     //     </div>
+    //     // </div>
+    //     )
 }
 export default ProductsList
